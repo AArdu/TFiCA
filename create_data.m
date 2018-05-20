5d357ccd8834894f06c1beb721f2e19d4fd122bf
AhA09080706 <aa.alessandro.ardu@gmail.com>
2018-05-20 10:32:16 +0200

    create workspace with initial gameboard

---
+++ create_data.m
@@ -0,0 +1,15 @@
+%% Open CSV file and import Data from initial board
+initialGameboard = csvimport('./Data/gameboard.csv');
+
+%% Get initial probabilities of the features (Features layer in BN)
+initialProbs = mean(cell2mat(initialGameboard(2:end, 2:end)), 1);
+
+initialGameboard(end+1, 2:end) = num2cell(initialProbs);
+initialGameboard(end, 1) = cellstr("initialProbs");
+
+% Sort the board by the probabilities of features
+[~, i] = sort(initialProbs, 'descend');
+sortedBoard = [initialGameboard(:, 1), initialGameboard(:, i+1)];
+
+%% Save workspace
+save("Data/initialGameboard.mat", 'initialGameboard', 'sortedBoard')
