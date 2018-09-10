39400e81e5e6aa6ce967c2538ceec4070df395bd
alessandro <aa.alessandro.ardu@gmail.com>
2018-09-10 18:36:15 +0200

    enter evidence manually, node by node

--- TEST_INFERENCE.m
+++ TEST_INFERENCE.m
@@ -28,4 +28,38 @@ ag.BN_engine = jtree_inf_engine(ag.bnet);
 
 evidence = cell(1, length(ag.bnet.node_sizes));
 
+evidence(1) = {[]}; % characters
+evidence(2) = {[]}; %
+evidence(3) = {[]}; %
+evidence(4) = {[]}; %
+evidence(5) = {[]}; %
+evidence(6) = {[]}; %
+evidence(7) = {[]}; %
+evidence(8) = {[]}; %
+evidence(9) = {[]}; %
+evidence(10) = {[]}; %
+evidence(11) = {[]}; %
+evidence(12) = {[]}; %
+evidence(13) = {[]}; %
+evidence(14) = {[]}; %
+evidence(15) = {[]}; %
+evidence(16) = {[]}; %
+evidence(17) = {[]}; %
+evidence(18) = {[]}; %
+evidence(19) = {[]}; %
+evidence(20) = {[]}; %
+evidence(21) = {[]}; %
+evidence(22) = {[]}; %
+evidence(23) = {[]}; %
+evidence(24) = {[]}; %
+evidence(25) = {[]}; %
+evidence(26) = {[]}; %
+evidence(27) = {[]}; %
+evidence(28) = {[]}; %
+evidence(29) = {2}; %
+
+
 [engine, loglike] = enter_evidence(ag.BN_engine, evidence);
+marg = marginal_nodes(engine, [8 9]);
+marg.T
+
